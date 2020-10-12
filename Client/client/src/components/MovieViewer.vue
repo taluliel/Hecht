68090885f00374ea4024521ffc08805656bfc9e3
arielbouskila <ariel.bouskila@gmail.com>
2020-10-12 20:50:07 +0300

    creating movie player removing helloworld componen

--- Client/client/src/components/MovieViewer.vue
+++ Client/client/src/components/MovieViewer.vue
@@ -1,34 +1,26 @@
 <template>
-<div class="questionCategories">
-    Question 1: 
-    <ol>
-        <li>
-            What?
-        </li>
-        <li>
-            What?
-        </li>
-        <li>
-            What?
-        </li>
-        <li>
-            What?
-        </li>
-        <li>
-            What?
-        </li>
-        
-    </ol>
+<div class="movieViewer">
+
+  <video width="450" controls  :src="movies[selectedMovie]"></video>
 </div>
 </template>
 
 <script>
+// import video1 from '../assets/video1.mp4';
+
 export default{
     name:'movieViewer',
     data:()=> ({
-      
+      movies:{
+          1:'https://www.w3schools.com/tags/movie.mp4'
+      },
+      selectedMovie:null
     }),
-    methods:{
+    mounted(){
+        this.selectedMovie = this.$route.params.id;
+        console.log(this.$route.params.id)
+    },
+    methods:{   
       goToRoom: function (){
         this.$router.push(`/room/${this.select}`).catch(err=>{err})
 
@@ -39,5 +31,9 @@ export default{
 </script>
 
 <style scoped>
-
+video {
+  /* override other styles to make responsive */
+  width: 100%    !important;
+  height: auto   !important;
+}
 </style>
--- Client/client/src/main.js
+++ Client/client/src/main.js
@@ -5,10 +5,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import Room from './components/Room';
 import QuestionViewer from './components/QuestionViewer'
-
+import MovieViewer from './components/MovieViewer';
 Vue.config.productionTip = false
 Vue.use(VueRouter)
-const routes = [{path:'/room/:id',name:'room',component:Room},{path:'/feature/:id',name:'feature',component:QuestionViewer}];
+const routes = [{path:'/room/:id',name:'room',component:Room},
+{path:'/feature/:id',name:'feature',component:QuestionViewer},
+{path:'/movie/:id',name:'movie',component:MovieViewer},
+];
 const router = new VueRouter({
   mode:'history',
   routes // short for `routes: routes`
